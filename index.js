590fbfa79329f50a6fa32be665f44441eef96e16
corbaz <julio.corbaz@gmail.com>
2019-07-22 14:56:42 -0300

    Video 4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 // npm install --save esm
 // nodemon -r esm index.js
 
@@ -5,14 +6,38 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port= process.env.PORT || 3000;
+const port = process.env.PORT || 4005;
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/api/product', (req, res) => {
+    res.status(200).send({
+        mensaje: 'Ruta GET product: OK',
+        products: []
+    });
 });
+
+app.get('/api/product/:productId', (req, res) => {
+    res.send(`Ruta GET product con parametro': ${req.params.productId}`);
+});
+
+app.post('/api/product', (req, res) => {
+    console.log(req.body);
+    res.status(200).send({mensaje: 'Ruta POST product OK'});
+});
+
+
+// Hace actualizaciones PUT
+app.put('/api/product/:productId', (req, res) => {
+    res.send(`Ruta: PUT product con parametro': ${res.params.productId}`);
+});
+
+// Hace borrar DELETE
+app.delete('/api/product/:productId', (req, res) => {
+    res.send(`Ruta PUT product con parametro': ${req.params.productId}`);
+});
+
 app.listen(port, () => {
-    console.log(`Api REST sobre el port: ${port}`)
+    console.log(`Api REST port: ${port}`)
 });
