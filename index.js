dabcd7bb21ea1bab8f013bfad8434230086a4b6e
corbaz <julio.corbaz@gmail.com>
2019-07-24 08:35:08 -0300

    JCC Producciones 3000

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = process.env.PORT || 4005;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
